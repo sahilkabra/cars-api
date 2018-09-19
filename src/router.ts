ba6047e0a07a88b09a41af1ad296ff8b74cf3141
Sahil Kabra <Sahil.Kabra@iress.com.au>
2018-09-19 13:53:26 +1000

    upload to gcp

--- src/router.ts
+++ src/router.ts
@@ -2,12 +2,14 @@ import * as express from 'express';
 
 import CarController from './car/controller';
 
-const router = express.Router();
-router.get('/hello', (req, res) => {
+const hello = (req: any, res: {json: () => object}) => {
     res.json({
         message: 'Hello there!',
     });
-});
+};
+const router = express.Router();
+router.get('/', hello);
+router.get('/hello', hello);
 router.post('/cars', CarController.create);
 router.get('/cars', CarController.getCars);
 router.get('/cars/:id', CarController.getCar);
